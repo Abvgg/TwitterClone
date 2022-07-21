447af754a9e44ae7179ec73e20f6c8d55df3b21b
Anna Scherbakova <anyasherbakova785@gmail.com>
2022-07-21 16:58:05 +0300

    add forms

--- twitterclone/twitter/views.py
+++ twitterclone/twitter/views.py
@@ -1,9 +1,21 @@
 import imp
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Profile, Post
-
+from .forms import UserRegisterForm
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts':posts}
     return render(request, 'twitter/newsfeed.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    context = {'form': form}
+    return render(request, 'twitter/register.html', context)
